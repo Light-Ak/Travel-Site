a17445d8619ee3dd02d1810271fff197d09b694c
Akash <ak26425617@gmail.com>
2025-10-20 00:34:35 +0530

    added some changes

--- controllers/users.js
+++ controllers/users.js
@@ -27,7 +27,7 @@ module.exports.handleRegister = async (req, res) => {
         const registeredUser = await User.register(newUser, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
-            req.flash('success', 'Welcome to Airbnb!');
+            req.flash('success', 'Welcome to Travel-Site!');
             res.redirect('/listings');
         });
     } catch (e) {
